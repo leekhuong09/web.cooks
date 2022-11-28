bdc776cd8f82e8893883e3594b18168761022209
Khuong Duy <ly.khuong1996@gmail.com>
2022-11-28 22:31:50 +0700

    .

--- src/components/Image/ImageDefault/index.js
+++ src/components/Image/ImageDefault/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import getConfig from 'next/config';
 
 import { Wrapper, Image } from './styled';
-
-const { publicRuntimeConfig } = getConfig();
 
 const ImageDefault = ({ src, children, ...otherProps }) => {
   return (
     <Wrapper>
-      <Image src={publicRuntimeConfig.DOMAIN_API + src} {...otherProps} />
+      <Image src={src} {...otherProps} />
       {children}
     </Wrapper>
   );
